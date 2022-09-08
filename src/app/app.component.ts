1d028407baa4f6864f87f3a6d4160a67a4dddeb0
David Bonser <Davidbonzer@hotmail.co.uk>
2022-09-08 10:50:47 +0100

    got value from service - need to export it to access it outside ngOnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ConfigService } from './config.service';
-import mapData from '../../db.json';
-
-interface mapDataJson {
-  latJson?: number;
-  lngJson?: number;
-}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'My First AGM Project';
-  public lat = 53.95;
-  public lng = -1.065;
+  public lat = 0;
+  public lng = 0;
   public latValue = this.lat;
   public lngValue = this.lng;
- // data: mapDataJson[] = mapData;
 
-  // this is trying to connect to the config.service but is not working
-  constructor(private service: ConfigService) {
-    this.service.getMapData();
+  constructor(private service: ConfigService) {}
+
+  ngOnInit(): void {
+    this.service.getLatData().subscribe((latdatajson) => {
+      console.log(latdatajson);
+    });
+    this.service.getLngData().subscribe((lngdatajson) => {
+      console.log(lngdatajson);
+    });
+  }
+
+  clickJson() {
+    console.log("send help");
   }
 
   updateLatitude() {
@@ -30,10 +33,6 @@ export class AppComponent {
   }
   updateLongitude() {
     this.lng = this.lngValue;
-  }
-
-  displayMapData() {
-    console.log(ConfigService);
   }
 }
 
--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -3,18 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { HammerGestureConfig } from '@angular/platform-browser';
-import mapData from '../../db.json';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConfigService {
   constructor(private http: HttpClient) {
-    this.getMapData().subscribe((mapData) => {
-      console.log(mapData);
-    });
   }
-  public getMapData(): Observable<any> {
-    return this.http.get('../../db.json');
+  public getLatData(): Observable<any> {
+    return this.http.get("http://localhost:3000/latJson");
+  }
+  public getLngData(): Observable<any> {
+    return this.http.get("http://localhost:3000/lngJson");
   }
 }
